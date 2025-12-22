275f540d22329ce7fad1485756af5c43f0d889e2
andreissh <shkudov.and@gmail.com>
2025-12-22 11:36:43 +0300

    add deps for ui kit; remove default code and styles; add appheader component content

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "./AppHeader.module.scss";
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 const AppHeader = () => {
-  return <div></div>;
+  return (
+    <div className={styles.headerWrapper}>
+      <div className={styles.headerContainer}>
+        <div className={styles.leftTabs}>
+          <div className={styles.tab}>
+            <BurgerIcon type="secondary" />
+            <span className={styles.tabTitle}>Конструктор</span>
+          </div>
+          <div className={styles.tab}>
+            <ListIcon type="secondary" />
+            <span className={styles.tabTitle}>Лента заказов</span>
+          </div>
+        </div>
+        <Logo className={styles.logo} />
+        <div className={styles.rightTabs}>
+          <div className={styles.tab}>
+            <ProfileIcon type="secondary" />
+            <span className={styles.tabTitle}>Личный кабинет</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppHeader;
