e3dacfc4226a7141d9faacc2f08ba16286d8f161
andreissh <shkudov.and@gmail.com>
2026-02-25 15:22:15 +0300

    add feed page layout

--- src/pages/OrderFeed/OrderFeed.tsx
+++ src/pages/OrderFeed/OrderFeed.tsx
@@ -1,8 +1,69 @@
 import React from "react";
 import styles from "./OrderFeed.module.scss";
+import clsx from "clsx";
+import Scrollbars from "rc-scrollbars";
 
 const OrderFeed = () => {
-  return <div className={styles.container}>Лента заказов</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>Лента заказов</h1>
+      <div className={styles.feedWrapper}>
+        <div className={styles.feedContainer}>
+          <Scrollbars style={{ width: "100%", height: 700 }}>
+            <ul className={styles.feedList}>
+              <li className={styles.feedItem}></li>
+              <li className={styles.feedItem}></li>
+              <li className={styles.feedItem}></li>
+              <li className={styles.feedItem}></li>
+              <li className={styles.feedItem}></li>
+            </ul>
+          </Scrollbars>
+        </div>
+        <div className={styles.feedInfoContainer}>
+          <div className={styles.feedOrdersBlock}>
+            <div className={styles.feedOrdersReady}>
+              <h2 className={styles.feedOrdersReadyTitle}>Готовы:</h2>
+              <ul className={styles.feedOrdersReadyList}>
+                <li
+                  className={clsx([
+                    styles.feedOrdersReadyItem,
+                    "iceland-regular",
+                  ])}
+                >
+                  034533
+                </li>
+              </ul>
+            </div>
+            <div className={styles.feedOrdersInProgress}>
+              <h2 className={styles.feedOrdersInProgressTitle}>В работе:</h2>
+              <ul className={styles.feedOrdersInProgressList}>
+                <li
+                  className={clsx([
+                    styles.feedOrdersInProgressItem,
+                    "iceland-regular",
+                  ])}
+                >
+                  034538
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.feedTotalBlock}>
+            <h2 className={styles.feedTotalTitle}>Выполнено за все время:</h2>
+            <span className={clsx([styles.feedTotalCount, "iceland-regular"])}>
+              28752
+            </span>
+          </div>
+          <div className={styles.feedTodayBlock}>
+            <h2 className={styles.feedTodayTitle}>Выполнено за сегодня:</h2>
+            <span className={clsx([styles.feedTodayCount, "iceland-regular"])}>
+              138
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderFeed;
